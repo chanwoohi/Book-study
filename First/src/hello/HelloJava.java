3c57fcb2e98b44d371c492ed218d99eae6fd3025
chanwoohi <chanwoohi@naver.com>
2024-05-28 17:48:40 +0900

    첫 연습

---
+++ First/src/hello/HelloJava.java
@@ -0,0 +1,14 @@
+/* 	첫번째 혼자 연습하기
+ 	2024년 5월 28일
+ 	첫 자바 복습입니다.
+ */
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");// 한 줄 주석 연습입니다.
+
+	}
+
+}
