9f395bed0fec37e23929c2ea31116b31b8716155
chanwoohi <chanwoohi@naver.com>
2024-05-29 17:48:18 +0900

    상수 사용 연습

---
+++ First/src/chapter2/Constant.java
@@ -0,0 +1,23 @@
+package chapter2;
+	//상수 사용
+public class Constant {
+	public static void main(String[] args) {
+		final int MAX_NUM = 100;
+		final int MIN_NUM;
+		
+		MIN_NUM = 0;
+		
+		System.out.println(MAX_NUM);
+		System.out.println(MIN_NUM);
+		
+		//MAX_NUM = 1000; 이미 상수로 정해져서 변경 불가능, 오류 떠서 주석 처리함
+		
+		
+		//상수 복습
+		
+		final int MY_AGE = 29;
+		System.out.println(MY_AGE);
+		
+		
+	}
+}
