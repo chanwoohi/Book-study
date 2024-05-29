41bc9567c5f967425706f4f1489d5563f323259d
chanwoohi <chanwoohi@naver.com>
2024-05-29 17:48:11 +0900

    실수형 연습

---
+++ First/src/chapter2/DoubleEx1.java
@@ -0,0 +1,11 @@
+package chapter2;
+	//실수형 연습
+public class DoubleEx1 {
+	public static void main(String[] args) {
+		double dnum = 3.14;
+		float fnum = 3.14F;
+
+		System.out.println(dnum);
+		System.out.println(fnum);
+	}
+}
