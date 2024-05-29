fa4e695d6d9cb33af44b5f507cbf7553ad9a63c3
chanwoohi <chanwoohi@naver.com>
2024-05-29 17:48:31 +0900

    문자형 연습1

---
+++ First/src/chapter2/CharacterEx1.java
@@ -0,0 +1,17 @@
+package chapter2;
+	//문자형 연습
+public class CharacterEx1 {
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println(ch1);
+		System.out.println((int)ch1);  // 아스키값?
+		
+		char ch2 = 66; // 정수 값 대입
+		System.out.println(ch2); // 정수 값에 해당하는 문자 출력
+		
+		int ch3 = 67;
+		System.out.println(ch3);
+		System.out.println((char)ch3);
+	}
+
+}
