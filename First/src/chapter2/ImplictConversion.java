462abe97a15e59d3695165628aaeaa46045f83b2
chanwoohi <chanwoohi@naver.com>
2024-05-30 17:46:27 +0900

    묵시적 형 변환 예제

--- First/src/chapter2/ImplictConversion.java
+++ First/src/chapter2/ImplictConversion.java
@@ -1,15 +1,15 @@
 package chapter2;
-
+	// 묵시적 형 변환
 public class ImplictConversion {
 	public static void main(String[] args) {
 		byte bNum = 10;
-		int iNum = bNum;
+		int iNum = bNum; // byte형 값이 int형 변수로 대입됨.
 		
 		System.out.println(bNum);
 		System.out.println(iNum);
 		
 		int iNum2 = 20;
-		float fNum = iNum2;
+		float fNum = iNum2; // int형 값이 float형 변수로 대입됨.
 		
 		System.out.println(iNum2);
 		System.out.println(fNum);
