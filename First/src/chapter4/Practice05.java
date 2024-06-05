6c3cebc46d43fea5494bfb3312553a7490af0d70
chanwoohi <chanwoohi@naver.com>
2024-06-05 09:00:09 +0900

    연습 문제 5 수정

--- First/src/chapter4/Practice05.java
+++ First/src/chapter4/Practice05.java
@@ -3,26 +3,25 @@ package chapter4;
 public class Practice05 {
 	//연습문제 5
 	public static void main(String[] args) {
-		int j ;
-		for( int i = 1 ; i <= 4 ; i++) {
+		int j, i;
+		for(  i = 1 ; i <= 4 ; i++) {
 			for( j = 1 ; j <= 4 - i ; j++ ) {
 				System.out.print(" ");
 			}
 			for( j = 1 ; j <= 2 * i - 1 ; j ++) {
 				System.out.print("*");
 			}
-			System.out.println(" ");
+			System.out.println("");
 		} 
-		for( int i = 2 ; i <= 4 ; i++) {
-			for( j = 1 ; j >= 4 - i ; j-- ) {
+		for( i = 1 ; i <= 3 ; i++) {
+			for( j = 1 ; j <= i  ; j++ ) {
 				System.out.print(" ");
 			}
-			for( j = 1 ; j >= 2 * i - 1 ; j-- ) {
+			for( j = 1 ; j <= 7 - 2 * i ; j++ ) {
 				System.out.print("*");
 			}
-			System.out.println(" ");
+			System.out.println("");
 		}
-// 수정 필요
 	}
 
 }
