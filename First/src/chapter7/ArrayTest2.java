524752dd503c0b8b42cf95766fb04848a35c1020
chanwoohi <chanwoohi@naver.com>
2024-06-05 13:45:56 +0900

    배열 길이만큼 출력하기

---
+++ First/src/chapter7/ArrayTest2.java
@@ -0,0 +1,14 @@
+package chapter7;
+
+public class ArrayTest2 {
+
+	public static void main(String[] args) {
+		// 배열 길이만큼 출력하기
+		double num [] = new double [] {10.0, 20.0, 30.0, 0, 0 };
+		for( int i = 0 ; i < num.length ; i++ ) {
+			System.out.println(num[i]);
+		}
+
+	}
+
+}
