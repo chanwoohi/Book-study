1a04e8a83566db87bc32eaa91be85f2652fb2b31
KimHyeonjung <kardel@naver.com>
2024-06-25 17:50:24 +0900

    나 혼자 코딩 해보기

---
+++ First/src/chapter7/StudentArray.java
@@ -0,0 +1,17 @@
+package chapter7;
+
+public class StudentArray {
+
+	public static void main(String[] args) {
+		Student [] list = new Student[3];
+		
+		list[0] = new Student ("1001", "James");
+		list[1] = new Student ("1002", "Tomas");
+		list[2] = new Student ("1003", "Edward");
+		
+		for(int i = 0 ; i < list.length ; i++ ) {
+			list[i].showStudentInfo();
+		}
+	}
+
+}
