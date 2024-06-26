79f36c1041515048f194a8f7753cc0c0d0c5f2e7
KimHyeonjung <kardel@naver.com>
2024-06-26 17:51:38 +0900

    subject 클래스 구현하기

---
+++ First/src/chapter7/Subject.java
@@ -0,0 +1,21 @@
+package chapter7;
+	
+//Subject 클래스 구현하기
+public class Subject {
+	private String name;
+	private int scorePoint;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScorePoint() {
+		return scorePoint;
+	}
+	public void setScorePoint(int scorePoint) {
+		this.scorePoint = scorePoint;
+	}
+	
+}
